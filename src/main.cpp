fccedb517945266fef01633049649a65c44f10b4
Ilya Ovchinnikov <ilyailyaovch@users.noreply.github.com>
2022-11-12 22:27:59 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
 #include <deque>
-#if 1 //CREATE A REAL STL EXAMPLE
+#if 0 //CREATE A REAL STL EXAMPLE
 	#include <map>
 	#include <stack>
 	#include <vector>
 	namespace ft = std;
 #else
-	#include <map.hpp>
-	#include <stack.hpp>
-	#include <vector.hpp>
+	#include "../containers/map.hpp"
+	#include "../containers/stack.hpp"
+	#include "../containers/vector.hpp"
 #endif
 
 #include <stdlib.h>
@@ -33,15 +33,15 @@ public:
 	MutantStack(const MutantStack<T>& src) { *this = src; }
 	MutantStack<T>& operator=(const MutantStack<T>& rhs) 
 	{
-		this->c = rhs.c;
+		this->_cont = rhs._cont;
 		return *this;
 	}
 	~MutantStack() {}
 
 	typedef typename ft::stack<T>::container_type::iterator iterator;
 
-	iterator begin() { return this->c.begin(); }
-	iterator end() { return this->c.end(); }
+	iterator begin() { return this->_cont.begin(); }
+	iterator end() { return this->_cont.end(); }
 };
 
 int main(int argc, char** argv) {
